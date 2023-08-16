3ddc12da5c2c2efd4b3b12db0ce7759b2ed80a12
purriin <v.joness1211@gmail.com>
2023-08-16 16:21:00 -0400

    Added transitions and more styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,9 @@ import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Product', href: '/about' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'About', href: '/about' },
+  { name: 'Experiences', href: '#' },
+  { name: 'Contact', href: '#' },
 ]
 
 export default function Example() {
@@ -16,7 +15,7 @@ export default function Example() {
     <div className="bg-gray-800">
       <header className="absolute inset-x-0 top-0 z-50">
 
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <nav className="relative flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
@@ -30,7 +29,7 @@ export default function Example() {
           <div className="flex lg:hidden">
             <button
               type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
+              className="transition-color ease-in-out duration-200 -m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white hover:text-gray-400"
               onClick={() => setMobileMenuOpen(true)}
             >
               <span className="sr-only">Open main menu</span>
@@ -39,11 +38,12 @@ export default function Example() {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 bg-gray-800">
+              <a key={item.name} href={item.href} className="transition-color ease-in-out duration-200 text-sm font-semibold leading-6 text-white hover:text-gray-400">
                 {item.name}
               </a>
             ))}
           </div>
+          
         </nav>
 
         <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
@@ -60,7 +60,7 @@ export default function Example() {
               </a>
               <button
                 type="button"
-                className="-m-2.5 rounded-md p-2.5 text-white"
+                className="transition-color ease-in-out duration-200 -m-2.5 rounded-md p-2.5 text-white hover:text-gray-400"
                 onClick={() => setMobileMenuOpen(false)}
               >
                 <span className="sr-only">Close menu</span>
@@ -74,19 +74,13 @@ export default function Example() {
                     <a
                       key={item.name}
                       href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-700"
+                      className="transition-color ease-in-out duration-200 -mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-700"
                     >
                       {item.name}
                     </a>
                   ))}
                 </div>
                 <div className="py-6">
-                  {/* <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-50"
-                  >
-                    Log in
-                  </a> */}
                 </div>
               </div>
             </div>
@@ -95,7 +89,7 @@ export default function Example() {
       </header>
 
       <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div
+        {/* <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
         >
@@ -106,20 +100,12 @@ export default function Example() {
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
           />
-        </div>
+        </div> */}
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-white ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-          </div>
-          <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-              Data to enrich your online business
+          
+          <div className="text-left">
+            <h1 className="text-10xl font-bold tracking-tight text-white sm:text-7xl">
+              Shannon Jones
             </h1>
             <p className="mt-6 text-lg leading-8 text-white">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
@@ -139,6 +125,30 @@ export default function Example() {
             </div> */}
 
           </div>
+
+          <div className="lg:py-10 hidden sm:mb-8 sm:flex sm:justify-left">
+            <div className="transition-color ease-in-out duration-200 relative rounded-full px-3 py-1 text-sm leading-6 text-white hover:text-gray-400 ring-1 ring-gray-500 hover:ring-gray-600">
+              Announcing our next round of funding.{' '}
+              <a href="#" className="transition-color ease-in-out duration-200 font-semibold text-indigo-600  hover:text-gray-600">
+                <span className="absolute inset-0" aria-hidden="true" />
+                Read more <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+
+          <div class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"> </div>
+
+          <div className="lg:py-20 text-left">
+            <h2 className="text-2xl font-bold tracking-tight text-white sm:text-5xl">
+              Skills
+            </h2>
+            <p className="mt-6 text-lg leading-8 text-white">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+              fugiat veniam occaecat fugiat aliqua.
+            </p>
+
+          </div>
+
         </div>
         {/* <div
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
